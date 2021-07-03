438d53873c913c5dfa8add2b0e05f8162eeee446
TharaniyaBalamugunthan <83435952+TharaniyaBalamugunthan@users.noreply.github.com>
2021-07-03 15:09:06 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var bg ;
 
 function preload() {
     // create getBackgroundImg( ) here
-    getBackgroundImage();
+    getBackgroundImg();
 }
 
 function setup(){
@@ -36,54 +36,52 @@ function draw(){
 async function getBackgroundImg(){
 
     // write code to fetch time from API
-    var response = await fetch("https://worldtimeapi.org/api/timezone/America/Toronto");
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
 
     //change the data in JSON format
     var responseJSON = await response.json();
     var datetime = responseJSON.datetime;
 
     // write code slice the datetime
-    var hour = datetime.slice(12,15);
-}
+    var hour = datetime.slice(11,13);
+
     // add conditions to change the background images from sunrise to sunset
     if(hour >= 04 && hour <= 06){
         bg = "sunrise1.png";
     }
-    else{ if(hour >= 06 && hour <= 08)
+    else if(hour >= 06 && hour <= 08){
         bg = "sunrise2.png";
-    
-    else { 
-        if(hour >= 08 && hour <= 10)
+    }
+    else if(hour >= 08 && hour <= 10){
         bg = "sunrise3.png";
-
-    else{ if(hour >= 10 && hour <= 12)
+    }
+    else if(hour >= 10 && hour <= 12){
          bg = "sunrise4.png";
-     
-    else{ if(hour >= 12 && hour <= 14)
+    }
+    else if(hour >= 12 && hour <= 14){
         bg = "sunrise5.png";
-    
-    else{ if(hour >= 14 && hour <= 16) 
+    }
+    else if(hour >= 14 && hour <= 16){ 
     bg = "sunrise6.png";
-   
-    else{ if(hour >= 16 && hour <= 18)
+    }
+    else if(hour >= 16 && hour <= 18){
     bg = "sunset7.png";
-   
-    else{ if(hour >= 18 && hour <= 20)
+    }
+    else if(hour >= 18 && hour <= 20){
     bg = "sunset8.png";
-   
-    else{ if(hour >= 20 && hour <= 22)
+    }
+    else if(hour >= 20 && hour <= 22){
     bg = "sunset9.png";
-   
-    else{ if(hour >= 22 && hour <= 24)
+    }
+    else if(hour >= 22 && hour <= 24){
     bg = "sunset10.png";
-   
-    else{ if(hour >= 24 && hour <= 02)
+    }
+    else if(hour >= 24 && hour <= 02){
     bg = "sunset11.png";
-    
-    else{ if(hour >= 02 && hour <= 06)
-    bg = "sunset8.png";
+    }
+    else if(hour >= 02 && hour <= 06){
+    bg = "sunset12.png";
     }
     //load the image in backgroundImg variable here
     backgroundImg = loadImage(bg);
-
-    }
+}
